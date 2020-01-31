00e1eef8c08e4a29044910d0d122ffba300b35e3
Ian Andrewson <ianandrewson@gmail.com>
2020-01-30 16:26:10 -0800

    HistoryList implemented. Updated Snapshot. Ready to implement state and props

---
+++ src/components/history/HistoryList.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import HistoryItem from './HistoryItem.js';
+
+const HistoryList = ({ onSelect, historyItems }) => {
+  const items = historyItems.map((item, i) => (
+    <li key={i} onSelect={onSelect}>
+      <HistoryItem {...item} />
+    </li>
+  ));
+
+  return (
+    <ul>
+      {items}
+    </ul>
+  );
+};
+
+HistoryList.propTypes = {
+  onSelect: PropTypes.func.isRequired,
+  historyItems: PropTypes.arrayOf(PropTypes.shape({
+    url: PropTypes.string.isRequired,
+    method: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired
+  })).isRequired
+};
+
+export default HistoryList
--- src/containers/Resty.js
+++ src/containers/Resty.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react';
 import ReqForm from '../components/form/ReqForm.js';
 import Result from '../components/Result.js';
+import HistoryList from '../components/history/HistoryList.js';
 
 export default class Resty extends Component {
+
+  state = {
+    historyItems: [],
+  }
+
   render(){
+    const { historyItems } = this.state;
+
     return (
       <>
         <ReqForm />
+        <HistoryList 
+          historyItems={historyItems} />
         <Result />
         <p>HELLO WORLD</p>
       </>
